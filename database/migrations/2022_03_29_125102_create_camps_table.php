fc198f5e493aae4a77bce9ea78713be3860f01b5
Nitoich <bcmamka@gmail.com>
2022-03-29 16:17:15 +0300

    Добавил миграции таблиц для БД и создал Модели

--- database/migrations/2022_03_29_125102_create_camps_table.php
+++ database/migrations/2022_03_29_125102_create_camps_table.php
@@ -13,11 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('camps', function (Blueprint $table) {
             $table->id();
-            $table->string('login')->unique();
-            $table->string('password');
-            $table->timestamps();
+            $table->string('name');
         });
     }
 
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('camps');
     }
 };
--- database/migrations/2022_03_29_125742_create_requests_table.php
+++ database/migrations/2022_03_29_125742_create_requests_table.php
@@ -13,10 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('requests', function (Blueprint $table) {
             $table->id();
-            $table->string('login')->unique();
-            $table->string('password');
+            $table->string('full_name');
+            $table->integer('old');
+            $table->foreignId('camp_id')->constrained('camps', 'id');
             $table->timestamps();
         });
     }
@@ -28,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('requests');
     }
 };
