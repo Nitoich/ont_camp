928650da4adb4c4bd9c1c36ff17cacffa48830a7
Nitoich <bcmamka@gmail.com>
2022-03-29 16:56:51 +0300

    Сверстал главную

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,34 @@
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Camp</title>
+        <link rel="stylesheet" href="/app.css">
     </head>
     <body>
+        <div class="main-container">
+            <div class="logo">
+                <img src="/images/logo.png" alt="">
+            </div>
+            <div class="title">
+                <h1>TITLE</h1>
+            </div>
+            <div class="form-container">
+                <div class="input-block">
+                    <input type="text" placeholder="ФИО">
+                </div>
+                <div class="input-block">
+                    <input type="text" placeholder="Возраст">
+                </div>
 
+                <div class="input-block">
+                    <select name="" id="">
+                        <option value="null" selected>Лагерь</option>
+                    </select>
+                </div>
+
+                <div class="input-block">
+                    <button>Отправить</button>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
